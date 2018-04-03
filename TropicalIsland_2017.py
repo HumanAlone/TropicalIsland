13901093a86808b3baa3433c2a1c83d9c7d3e9aa
HumanAlone <humanalone@ya.ru>
2018-04-03 21:42:47 +0300

    commit

--- TropicalIsland_2017.py
+++ TropicalIsland_2017.py
@@ -1,3 +1,122 @@
+def get_water_volume(island):
+    M = len(island)
+    # Число строк матрицы
+    N = len(island[0])
+
+    # Вторая строка и столбец, нумерация с нуля
+    i = 1
+    j = 1
+    # Низина
+    lowland = {}
+    # Граница низины
+    lowland_boundary = {}
+    lowland_boundary_new = {}
+    # Объём воды
+    volume = 0
+
+    for list_ in island[1:M - 1]:
+
+        for element in list_[1:N - 1]:
+
+            temp_lowland = {}
+
+            def searching_cross(element, i, j):
+                prep_volume = 0
+
+                # print((i, j))
+                if element < island[i][j - 1] \
+                        and element < island[i - 1][j] \
+                        and element < island[i][j + 1] \
+                        and element < island[i + 1][j] and (i, j) not in lowland:
+                    lowland[i, j] = element
+                    # Разница между текущим элементом и его минимальным соседом
+                    delta = min([island[i][j - 1],
+                                 island[i - 1][j],
+                                 island[i][j + 1],
+                                 island[i + 1][j]]) - element
+                    # Низина затопляется
+                    lowland[i, j] += delta
+                    island[i][j] += delta
+                    prep_volume += delta
+
+                if element >= island[i][j - 1] and (i, j) not in lowland:
+                    lowland[i, j] = element
+                    temp_lowland[i, j] = element
+                    lowland_boundary[i - 1, j] = island[i - 1][j]
+                    lowland_boundary[i, j + 1] = island[i][j + 1]
+                    lowland_boundary[i + 1, j] = island[i + 1][j]
+                    lowland_boundary[i, j - 1] = island[i][j - 1]
+                    if j - 1 != 0:
+                        return searching_cross(island[i][j - 1], i, j - 1)
+
+                if element >= island[i - 1][j] and (i, j) not in lowland:
+                    lowland[i, j] = element
+                    temp_lowland[i, j] = element
+                    lowland_boundary[i, j - 1] = island[i][j - 1]
+                    lowland_boundary[i, j + 1] = island[i][j + 1]
+                    lowland_boundary[i + 1, j] = island[i + 1][j]
+                    lowland_boundary[i - 1, j] = island[i - 1][j]
+                    if i - 1 != 0:
+                        return searching_cross(island[i - 1][j], i - 1, j)
+
+                if element >= island[i][j + 1] and (i, j) not in lowland:
+                    lowland[i, j] = element
+                    temp_lowland[i, j] = element
+                    lowland_boundary[i, j - 1] = island[i][j - 1]
+                    lowland_boundary[i - 1, j] = island[i - 1][j]
+                    lowland_boundary[i + 1, j] = island[i + 1][j]
+                    lowland_boundary[i, j + 1] = island[i][j + 1]
+                    if j + 1 != N - 1:
+                        return searching_cross(island[i][j + 1], i, j + 1)
+
+                if element >= island[i + 1][j] and (i, j) not in lowland:
+                    lowland[i, j] = element
+                    temp_lowland[i, j] = element
+                    lowland_boundary[i, j - 1] = island[i][j - 1]
+                    lowland_boundary[i - 1, j] = island[i - 1][j]
+                    lowland_boundary[i, j + 1] = island[i][j + 1]
+                    lowland_boundary[i + 1, j] = island[i + 1][j]
+                    if i + 1 != M - 1:
+                        return searching_cross(island[i + 1][j], i + 1, j)
+
+                # Минимальное число в границе низин
+                for key, value in lowland_boundary.items():
+                    if key not in temp_lowland:
+                        lowland_boundary_new[key] = value
+
+                try:
+                    min_ = min(lowland_boundary_new.values())
+                except ValueError:
+                    pass
+                else:
+                    for key, value in temp_lowland.items():
+                        if value > min_:
+                            continue
+                        delta = min_ - value
+                        prep_volume += delta
+                        lowland[key] += delta
+
+                        island[key[0]][key[1]] += delta
+
+                temp_lowland.clear()
+                lowland_boundary_new.clear()
+                lowland_boundary.clear()
+                return prep_volume
+
+            volume += searching_cross(element, i, j)
+
+            j += 1
+
+        j = 1
+        i += 1
+
+    # for x in island:
+    #   print(x)
+    while volume:
+        return volume + get_water_volume(island)
+
+    return volume
+
 islands = [
     [
         [3, 2, 8, 5, 4, 7],
@@ -140,127 +259,6 @@ islands = [
         [5, 4, 1, 7, 10]
     ]
 ]
-
-
-def get_water_volume(island):
-    M = len(island)
-    # Число строк матрицы
-    N = len(island[0])
-
-    # Вторая строка и столбец, нумерация с нуля
-    i = 1
-    j = 1
-    # Низина
-    lowland = {}
-    # Граница низины
-    lowland_boundary = {}
-    lowland_boundary_new = {}
-    # Объём воды
-    volume = 0
-
-    for list_ in island[1:M - 1]:
-
-        for element in list_[1:N - 1]:
-
-            temp_lowland = {}
-
-            def searching_cross(element, i, j):
-                prep_volume = 0
-
-                # print((i, j))
-                if element < island[i][j - 1] \
-                        and element < island[i - 1][j] \
-                        and element < island[i][j + 1] \
-                        and element < island[i + 1][j] and (i, j) not in lowland:
-                    lowland[i, j] = element
-                    # Разница между текущим элементом и его минимальным соседом
-                    delta = min([island[i][j - 1],
-                                 island[i - 1][j],
-                                 island[i][j + 1],
-                                 island[i + 1][j]]) - element
-                    # Низина затопляется
-                    lowland[i, j] += delta
-                    island[i][j] += delta
-                    prep_volume += delta
-
-                if element >= island[i][j - 1] and (i, j) not in lowland:
-                    lowland[i, j] = element
-                    temp_lowland[i, j] = element
-                    lowland_boundary[i - 1, j] = island[i - 1][j]
-                    lowland_boundary[i, j + 1] = island[i][j + 1]
-                    lowland_boundary[i + 1, j] = island[i + 1][j]
-                    lowland_boundary[i, j - 1] = island[i][j - 1]
-                    if j - 1 != 0:
-                        return searching_cross(island[i][j - 1], i, j - 1)
-
-                if element >= island[i - 1][j] and (i, j) not in lowland:
-                    lowland[i, j] = element
-                    temp_lowland[i, j] = element
-                    lowland_boundary[i, j - 1] = island[i][j - 1]
-                    lowland_boundary[i, j + 1] = island[i][j + 1]
-                    lowland_boundary[i + 1, j] = island[i + 1][j]
-                    lowland_boundary[i - 1, j] = island[i - 1][j]
-                    if i - 1 != 0:
-                        return searching_cross(island[i - 1][j], i - 1, j)
-
-                if element >= island[i][j + 1] and (i, j) not in lowland:
-                    lowland[i, j] = element
-                    temp_lowland[i, j] = element
-                    lowland_boundary[i, j - 1] = island[i][j - 1]
-                    lowland_boundary[i - 1, j] = island[i - 1][j]
-                    lowland_boundary[i + 1, j] = island[i + 1][j]
-                    lowland_boundary[i, j + 1] = island[i][j + 1]
-                    if j + 1 != N - 1:
-                        return searching_cross(island[i][j + 1], i, j + 1)
-
-                if element >= island[i + 1][j] and (i, j) not in lowland:
-                    lowland[i, j] = element
-                    temp_lowland[i, j] = element
-                    lowland_boundary[i, j - 1] = island[i][j - 1]
-                    lowland_boundary[i - 1, j] = island[i - 1][j]
-                    lowland_boundary[i, j + 1] = island[i][j + 1]
-                    lowland_boundary[i + 1, j] = island[i + 1][j]
-                    if i + 1 != M - 1:
-                        return searching_cross(island[i + 1][j], i + 1, j)
-
-                # Минимальное число в границе низин
-                for key, value in lowland_boundary.items():
-                    if key not in temp_lowland:
-                        lowland_boundary_new[key] = value
-
-                try:
-                    min_ = min(lowland_boundary_new.values())
-                except ValueError:
-                    pass
-                else:
-                    for key, value in temp_lowland.items():
-                        if value > min_:
-                            continue
-                        delta = min_ - value
-                        prep_volume += delta
-                        lowland[key] += delta
-
-                        island[key[0]][key[1]] += delta
-
-                temp_lowland.clear()
-                lowland_boundary_new.clear()
-                lowland_boundary.clear()
-                return prep_volume
-
-            volume += searching_cross(element, i, j)
-
-            j += 1
-
-        j = 1
-        i += 1
-
-    # for x in island:
-    #   print(x)
-    while volume:
-        return volume + get_water_volume(island)
-
-    return volume
-
 
 for island in islands:
     print(get_water_volume(island))
